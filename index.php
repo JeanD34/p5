4acbda7e53987f6bbb8ba8cc55bc62f480a1d15d
jeanw <jeanw@MSI.home>
2018-12-09 17:04:56 +0100

    test

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
 
-require 'modele.php';
+require 'controleur/controleur.php';
 
 try {
-    $articles = getArticles();
-    require 'vueAccueil.php';
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'article') {
+            if(isset($_GET['id'])) {
+                article($_GET['id']);
+            } else {
+                throw new Exception('Numéro article inexistant');
+            }
+        } else {
+            throw new Exception('Action invalide');
+        }
+    } else {
+        accueil();
+    }
     
 } catch (Exception $e) {
-    $msgErreur = $e->getMessage();
-    require 'vueErreur.php';
+    erreur($e->getMessage());
 }
-
-
 ?>
 
 
--- vueErreur.php
+++
@@ -1,6 +0,0 @@
-<?php $titre = 'Test'; ?>
-
-<?php ob_start(); ?>
-<p>Une erreur est survenue : <?= $msgErreur ?></p>
-<?php $contenu = ob_get_clean(); ?>
-<?php  require 'gabarit.php'; ?>
