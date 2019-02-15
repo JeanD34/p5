d2cdf592d8991d7072118d593606a1bdb4d46fb1
JeanD34 <jean.webdev@gmail.com>
2019-02-15 19:38:06 +0100

    Add avatar validation function

--- Validator/Validator.php
+++ Validator/Validator.php
@@ -30,5 +30,20 @@ class Validator {
                 return basename($params['name']);
             }
         }        
-    }    
+    }
+    
+    public static function validateAvatar($params)
+    {            
+        if ($params['size'] <= 1000000) {
+            
+            $file_info = pathinfo($params['name']);
+            $upload_extension = $file_info['extension'];
+            $valid_extension = array('jpg', 'jpeg', 'gif', 'png');
+            if (in_array($upload_extension, $valid_extension)) {
+                
+                move_uploaded_file($params['tmp_name'], 'C:/wamp64/www/eclipse/test_mvc_poo/Content/images/' . basename($params['name']));
+                return basename($params['name']);
+            }
+        }        
+    }
 }
