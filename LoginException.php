381f6d3d941c177e8c40242fc267e38cceb4be5e
JeanD34 <jean.webdev@gmail.com>
2019-02-15 19:34:31 +0100

    Remove header redirection

--- LoginException.php
+++ LoginException.php
@@ -5,8 +5,5 @@ class LoginException extends Exception
     public function __construct($message=NULL, $code=0)
     {
         parent::__construct($message, $code);
-        /*header("Location: ?action=loginView&error=$message");*/
-    }
-    
-    
+    }    
 }
