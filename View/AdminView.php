91757b5caee1d9f1004570cef2246d4140db0592
JeanD34 <jean.webdev@gmail.com>
2019-02-17 11:44:45 +0100

    Update view with new variables

--- View/AdminView.php
+++ View/AdminView.php
@@ -15,15 +15,15 @@
                 <div class="card card-small card-post card-post--1">
                   <div class="card-post__image" style="background-image: url('Content/images/<?= $post->getImage(); ?>');">                  
                     <div class="card-post__author d-flex">
-                      <a href="#" class="card-post__author-avatar card-post__author-avatar--small" style="background-image: url('Content/backend/images/avatars/0.jpg');">Written by Anna Kunis</a>
+                      <a href="?action=userProfile&id=<?= $post->getId_user_fk(); ?>" target="_blank" class="card-post__author-avatar card-post__author-avatar--small" style="background-image: url('Content/backend/images/avatars/<?= $post->getAvatar(); ?>');">Ecrit par <?= $post->getUsername(); ?></a>
                     </div>
                   </div>
                   <div class="card-body">
                     <h5 class="card-title">
-                      <a class="text-fiord-blue" href="?action=updateView&id=<?= $post->getId(); ?>"><?= $post->getTitle(); ?></a>
+                      <a class="text-fiord-blue" href="?action=updateView&id=<?= $post->getId(); ?>" target="_blank"><?= $post->getTitle(); ?></a>
                     </h5>
                     <p class="card-text d-inline-block mb-3"><?= $post->getLead(); ?></p><br>
-                    <span class="text-muted">28 February 2019</span>
+                    <span class="text-muted"><?= ucfirst(utf8_encode(strftime('%A %e %B %Y', strtotime($post->getAdd_date())))); ?></span>
                     <div class="blog-comments__actions mt-3">
                       <div class="btn-group btn-group-sm">
                         <button type="submit" class="btn btn-white">
@@ -34,7 +34,7 @@
                           <span class="text-danger">
                             <i class="material-icons">clear</i>
                           </span> Supprimer </button>
-                          <a href="?action=post&id=<?= $post->getId(); ?>">
+                          <a href="?action=post&id=<?= $post->getId(); ?>" target="_blank">
                     		<button class="btn btn-white">
                               <span class="text-light">
                                 <i class="material-icons">arrow_right_alt</i>
@@ -56,18 +56,23 @@
                   <div class="col-lg-5 col-md-12 col-sm-12 mb-4">
                     <div class="card card-small blog-comments">
                       <div class="card-header border-bottom">
-                        <h6 class="m-0">Commentaires</h6>
+                        <h6 class="m-0">Commentaires en attente de validation</h6>
                       </div>
                       <div class="card-body p-0">
                       <?php foreach ($commentToValidate as $comment) : ?>
+                      <?php 
+                      $dateComment = new DateTime($comment->getAdd_date());
+                      $now = new DateTime();
+                      $dayComment = $dateComment->diff($now)->format("%d");    
+                      ?>
                         <div class="blog-comments__item d-flex p-3">
                           <div class="blog-comments__avatar mr-3">
-                            <img src="Content/backend/images/avatars/1.jpg" alt="User avatar" /> </div>
+                            <img src="Content/backend/images/avatars/<?= $comment->getAvatar(); ?>" alt="Avatar" /> </div>
                           <div class="blog-comments__content">
                             <div class="blog-comments__meta text-muted">
-                              <a class="text-secondary" href="#">Pseudo</a> sur
-                              <a class="text-secondary" href="#">Nom Article</a>
-                              <span class="text-muted">– il y a "x" jours</span>
+                              <a class="text-secondary" href="?action=userProfile&id=<?= $comment->getId_user_fk(); ?>"><?= $comment->getUsername(); ?></a> sur
+                              <a class="text-secondary" href="?action=post&id=<?= $comment->getId_post_fk(); ?>" target="_blank"><?= $comment->getTitle(); ?></a>
+                              <span class="text-muted">– il y a <?= $dayComment ?> jours</span>
                             </div>
                             <p class="m-0 my-1 mb-2 text-muted"><?= $comment->getContent(); ?></p>
                             <div class="blog-comments__actions">
