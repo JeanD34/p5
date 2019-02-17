257c7c6233b489a6731a3c4567347e1df40edfc6
JeanD34 <jean.webdev@gmail.com>
2019-02-17 11:33:23 +0100

    New attribute after some changes in db and add Setters and Getters corresponding

--- Entity/Comment.php
+++ Entity/Comment.php
@@ -5,8 +5,12 @@ class Comment
     use HydratorTrait;
     private $id;
     private $content;
+    private $add_date;
+    private $username;
+    private $avatar;
     private $id_post_fk;
     private $id_user_fk;
+    private $title;
     
     // Setters
     
@@ -19,7 +23,22 @@ class Comment
     {
         $this->content = $content;
     }
+
+    public function setAdd_date($add_date) 
+    {
+        $this->add_date = $add_date;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
     
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+    }
+
     public function setId_post_fk($id_post_fk)
     {
         $this->id_post_fk = $id_post_fk;
@@ -30,6 +49,11 @@ class Comment
         $this->id_user_fk = $id_user_fk;
     }
     
+    public function setTitle($title) 
+    {
+        $this->title = $title;
+    }
+
     // Getters
     
     public function getId()
@@ -40,7 +64,23 @@ class Comment
     public function getContent()
     {
         return $this->content;
-    } 
+    }
+    
+    public function getAdd_date() 
+    {
+        
+        return $this->add_date;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
     
     public function getId_post_fk()
     {
@@ -50,5 +90,10 @@ class Comment
     public function getId_user_fk()
     {
         return $this->id_user_fk;
+    }
+
+    public function getTitle()
+    {
+        return strtoupper($this->title);
     }
 }
