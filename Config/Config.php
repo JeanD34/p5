9d29eac02f08f4a86a5967a7009966a0fa59d1be
JeanD34 <jean.webdev@gmail.com>
2019-02-17 11:19:39 +0100

    Add constant for autoload path and confirm email link

--- Config/Config.php
+++ Config/Config.php
@@ -12,3 +12,10 @@ define('PASS', '');
 define('BLOG_POST_LIMIT', '6');
 define('ADMIN_POST_LIMIT', '8');
 
+// Path
+
+define('AUTOLOAD_PATH', 'blog'); // Folder who contains the website
+
+// Confirm Mail
+
+define('CONFIRM_MAIL_LINK', 'http://localhost/blog/'); // URL from your website
