de5f6bdf61e368b70e45e78d333bd5cf1024dec9
JeanD34 <jean.webdev@gmail.com>
2019-02-17 16:47:26 +0100

    Add img and a home button

--- View/ErrorView.php
+++ View/ErrorView.php
@@ -1,6 +1,8 @@
-<?php $this->title = 'Test'; ?>
+<?php $this->title = 'Erreur'; ?>
 
 <div class="paddsections text-center">
     <p class="error">Une erreur est survenue : <?= $errorMsg ?></p>
 </div>
+<div class="text-center"><img class="error-img" src="Content/images/error.png"></div>
+<div class="text-center"><a href="index.php"><button class="btn">Retour à l'accueil</button></a></div>
 
